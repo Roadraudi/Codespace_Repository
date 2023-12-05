e3cf921f42a75247f3368628541940c1d385d8d1
Felix Eder <fel.eder.23@lehre-mosbach.dhbw.de>
2023-12-05 20:33:10 +0100

    Lebenslauf Generator

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+name = input("Wie lautet der Name: ")
+own_name = "Felix"
+age = 20
+city = "Schwarzach"
+is_men = True
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if(is_men == False ):
+    print("Sehr geehrte Frau " + name + ",")
+else:
+    print("Sehr geehrter Herr " + name + ",")
+print(" ")
+print("Ich heiße " + own_name + ".")
+print("Ich bin " + age + " und komme aus " + city + ".")
