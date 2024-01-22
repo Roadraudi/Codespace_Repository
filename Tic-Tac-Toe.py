d250facd329afc266a8d2c019e465be26c8bfb8e
Roadraudi <152898759+Roadraudi@users.noreply.github.com>
2024-01-22 14:21:49 +0100

    Commit of 22.01.24

---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,13 @@
+spielbrett = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+]
+
+def show_spielbrett():
+    for zeile in spielbrett:
+        print("|".join(zeile))
+        print("_____")
+
+show_spielbrett()
+
