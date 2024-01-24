baa071daae78efaa8c9e2a59e4c44b23a211d430
Roadraudi <152898759+Roadraudi@users.noreply.github.com>
2024-01-24 14:35:51 +0100

    Commit of 24.01.24

--- src/Tic-Tac-Toe.py
+++ src/Tic-Tac-Toe.py
@@ -9,4 +9,20 @@ def show_spielbrett():
         print("|".join(zeile))
         print("_____")
 
+def play():
+    player1 = True
+    eingabe = [
+        [], []
+    ]
+    if player1 == True:
+        print("Spieler 1 ist an der Reihe")
+        print("Setze dein X!", end=' ')
+        eingabe = input("Spalte|Zeile")
+        player1 = False
+    else:
+        print("Spieler 2 ist an der Reihe")
+        eingabe = input("Setze dein 0!(Spalte|Zeile)")
+        player1 = True
+
 show_spielbrett()
+play()
